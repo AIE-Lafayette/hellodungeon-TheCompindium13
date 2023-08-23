98101ecc5c0e54f5d662261f1b2d1b1f3c486b6b
Haidon Cosse <s238005@students.aie.edu>
2023-08-23 15:05:55 -0500

    added strings, bool, int, float, and first sentence.

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -11,6 +11,22 @@ namespace HelloDungeon
         public void Run()
         {
 
+            string DogName = "Rex";
+            string PlayerName = "";
+            bool IsAlive = true;
+            int IntelPoints = 10;
+            float HealthPoints = 32.76f;
+            int AreaNumber = 0;
+            
+            Console.WriteLine("Fin Yah");
+            Console.WriteLine("'Drem yol lok Adventurer Wo are You?'");
+            
+            string firstname = Console.ReadLine();
+            string lastname = Console.ReadLine();
+            PlayerName = firstname + lastname;
+
+            Console.WriteLine("'Drem yol lok great Hun " + PlayerName + ".'");
+
         }
     }
 }
