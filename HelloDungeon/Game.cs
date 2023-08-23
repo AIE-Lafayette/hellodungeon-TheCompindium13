fa7afe1eb2f6acef62a1407f5d8b105f988eba18
Haidon Cosse <s238005@students.aie.edu>
2023-08-23 16:55:15 -0500

    no change

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -10,21 +10,25 @@ namespace HelloDungeon
     {
         public void Run()
         {
-
+            
+            //Starting strings
             string DogName = "Rex";
             string PlayerName = "";
             bool IsAlive = true;
             int IntelPoints = 10;
-            float HealthPoints = 32.76f;
+            float HealthPoints = 32.76f * 2;
             int AreaNumber = 0;
             
+            //Start writing
             Console.WriteLine("Fin Yah");
             Console.WriteLine("'Drem yol lok Adventurer Wo are You?'");
             
+            //Name
             string firstname = Console.ReadLine();
             string lastname = Console.ReadLine();
             PlayerName = firstname + lastname;
 
+            //Printed name
             Console.WriteLine("'Drem yol lok great Hun " + PlayerName + ".'");
 
         }
