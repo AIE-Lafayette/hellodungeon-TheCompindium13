a37ceddd9f5655ca8916eb8741930f23d8039f78
Haidon Cosse <s238005@students.aie.edu>
2023-09-01 11:58:37 -0500

    continuing with current code clean-up and adding functions

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -8,23 +8,28 @@ namespace HelloDungeon
 {
     class Game
     {
+        //Stats Menu
         void PrintStats(string name, float health, float intel, float strength, float charisma, float luck, float stamina, float percept)
         {
-            Console.WriteLine("Courior Name = " + name);
-            Console.WriteLine("Health = " + health);
-            Console.WriteLine("Smarts = " + intel);
-            Console.WriteLine("Strength = " + strength);
-            Console.WriteLine("Stamina = " + stamina);
-            Console.WriteLine("Charisma = " + charisma);
-            Console.WriteLine("Luck = " + luck);
-            Console.WriteLine("Perception = " + percept);
+            while()
+            {
+                Console.WriteLine("Courior Name = " + name);
+                Console.WriteLine("Health = " + health);
+                Console.WriteLine("Smarts = " + intel);
+                Console.WriteLine("Strength = " + strength);
+                Console.WriteLine("Stamina = " + stamina);
+                Console.WriteLine("Charisma = " + charisma);
+                Console.WriteLine("Luck = " + luck);
+                Console.WriteLine("Perception = " + percept);
+            }
+            
         }
         
-        string DisplayMenu(string prompt, string option1, string option2, string option3)
+        string DisplayMenu(string prompt, string option1, string option2, string option3, string option4, string option5, string option6)
         {
             string PlayerChoice = "";
             
-            while (PlayerChoice != "1" && PlayerChoice != "2" && PlayerChoice != "3")
+            while (PlayerChoice != "1" && PlayerChoice != "2" && PlayerChoice != "3" && PlayerChoice != "4" && PlayerChoice != "5" && PlayerChoice != "6")
             {
                 //Display Prompt
                 Console.Clear();
@@ -33,22 +38,53 @@ namespace HelloDungeon
                 //Display All Options
                 Console.WriteLine("1." + option1);
                 Console.WriteLine("2." + option2);
-                Console.WriteLine("3." + option3);
-                
+                if (option3 != "")
+                {
+                    Console.WriteLine("3." + option3);
+                }
+                if (option4 != "")
+                {
+                    Console.WriteLine("4." + option4);
+                }
+                if (option5 != "")
+                {
+                    Console.WriteLine("5." + option5);
+                }
+                if (option6 != "")
+                {
+                    Console.WriteLine("6." + option6);
+                }
                 //Get Player Input
                 Console.Write("<");
                 PlayerChoice = Console.ReadLine();
 
                 //If Not Then...
-                if (PlayerChoice != "1" && PlayerChoice != "2" && PlayerChoice != "3")
+                if (PlayerChoice != "1" && PlayerChoice != "2")
                 {
+                    if (PlayerChoice == "3" && option3 != "")
+                    {
+                        continue;
+                    }
+                    if (PlayerChoice == "4" && option4 != "")
+                    {
+                        continue;
+                    }
+                    if (PlayerChoice == "5" && option5 != "")
+                    {
+                        continue;
+                    }
+                    if (PlayerChoice == "6" && option6 != "")
+                    {
+                        continue;
+                    }
+
+
                     //...Display ERROR Message
                     Console.Clear();
                     Console.WriteLine("Wrong");
                     Console.WriteLine("Press any key to continue...");
                     Console.ReadKey(true);
-
-
+                    PlayerChoice = "";
                 }
             }
             return PlayerChoice;
@@ -64,7 +100,7 @@ namespace HelloDungeon
 
             string DogName = "Rex";
             string PlayerName = "";
-            string Playerchoice = "";
+            string PlayerChoice = "";
             bool IsAlive = true;
             float IntelPoints = 10.00f;
             float StrengthPoints = 10.00f;
@@ -79,44 +115,34 @@ namespace HelloDungeon
 
 
 
-
+            //Game Start
             while (GameIsOn == true)
             {
                 
 
-                Playerchoice = "";
-                while (Playerchoice != "1")
+                while (PlayerChoice != "1")
                 {
                     Console.ReadKey();
                     Console.WriteLine("Tales of a Well Traveled Courior");
                     Console.ReadLine();
                     Console.WriteLine("'What is your name?'");
-
-
-
                     Console.Write(">");
 
                     //Name
-                    
                     string firstname = Console.ReadLine();
                     string lastname = Console.ReadLine();
                     PlayerName = firstname + lastname;
 
-                    Console.WriteLine("'Your name is " + PlayerName + " correct?'");
-
-                    //Confirming The Choice
-                    
-                    Console.WriteLine("1. Yes");
-                    Console.WriteLine("2. No");
-
-                    Playerchoice = Console.ReadLine();
+                    //First Player Choice
+                    PlayerChoice = DisplayMenu("'Your name is " + PlayerName + " correct?'", "Yes", "No", "", "", "", "");
+                    PlayerChoice = Console.ReadLine();
                     Console.Clear();
-                    if (Playerchoice == "2")
+                    if (PlayerChoice == "2")
                     {
                         Console.WriteLine("'I am sorry please can you repeat it?'");
 
                     }
-                    else if (Playerchoice == "1")
+                    else if (PlayerChoice == "1")
                     {
                         Console.WriteLine("'Great now let's begin'");
 
@@ -131,8 +157,8 @@ namespace HelloDungeon
 
 
                 //String Code
-                
-                string PlayerChoice = Console.ReadLine();
+
+                PlayerChoice = Console.ReadLine();
 
                 //Worldbuilding
                 
@@ -144,24 +170,24 @@ namespace HelloDungeon
                 Console.WriteLine("various areas across the southwest wasteland.");
                 Console.WriteLine("You are to be paid on delivery. Your first delivery is in the town Lafayette");
                 Console.WriteLine("The caravan is stopping for a moment to refuel.");
-                Console.WriteLine("To pass the time you...");
+                Console.ReadLine();
 
-                //First Action
-                PlayerChoice = DisplayMenu("To pass the time you...", "1. Talk with a caravan hand", "2. Barter with the dog trader", "3. Take a nap");
+                    //First Action
+                    PlayerChoice = DisplayMenu("To pass the time you...", "Talk with a caravan hand", "Barter with the dog trader", "Take a nap", "Sleep", "Wait around", "Check stats");
 
-                while (PlayerChoice != "1" && PlayerChoice != "2" && PlayerChoice != "3" && PlayerChoice != "4" && PlayerChoice != "5" && PlayerChoice != "6")
-                {
-                    //Story Start
+                //while (PlayerChoice != "1" && PlayerChoice != "2" && PlayerChoice != "3" && PlayerChoice != "4" && PlayerChoice != "5" && PlayerChoice != "6")
+                //{
+                //    //Story Start
                     
-                    //Console.ReadLine();
-                    //Console.WriteLine("1. Talk with a caravan hand");
-                    //Console.WriteLine("2. Barter with the dog trader");
-                    //Console.WriteLine("3. Take a nap");
-                    //Console.WriteLine("4. Sleep");
-                    //Console.WriteLine("5. Wait around");
-                    //Console.WriteLine("6. Check stats");
+                //    //Console.ReadLine();
+                //    //Console.WriteLine("1. Talk with a caravan hand");
+                //    //Console.WriteLine("2. Barter with the dog trader");
+                //    //Console.WriteLine("3. Take a nap");
+                //    //Console.WriteLine("4. Sleep");
+                //    //Console.WriteLine("5. Wait around");
+                //    //Console.WriteLine("6. Check stats");
                     
-                    //Conversation Star
+                    //Conversation Start
                     if (PlayerChoice == "1")
                     {
                         Console.WriteLine("You learn about a local legend about a strange armored devil.");
@@ -190,7 +216,6 @@ namespace HelloDungeon
                     else if (PlayerChoice == "6")
                     {
                         PrintStats(PlayerName, HealthPoints, IntelPoints, StrengthPoints, CharismaPoints, LuckPoints, StaminaPoints, PerceptionPoints);
-
                     }
                     Console.ReadLine();
                     Console.Clear();
@@ -205,27 +230,44 @@ namespace HelloDungeon
                     Console.WriteLine("You have finally arrived at your first point of delivery... the city of Lafayette.");
                     Console.WriteLine("The instructions on the parcel says to bring the package to Old Ann's Church");
                     Console.WriteLine(" but no other directions are stated... Strange.");
+                    
 
-                    PlayerChoice = "";
-                    while(PlayerChoice != "1" && PlayerChoice != "2")
-                    {
-                        Console.WriteLine("Restart?");
-                        Console.WriteLine("1. Yes");
-                        Console.WriteLine("2. No");
-                        Console.Write("<");
 
-                        if (PlayerChoice == "1")
-                        {
-                            GameIsOn = true;
-                        }
-                        else if (PlayerChoice == "2");
-                        {
-                            Console.WriteLine("Goodbye");
-                            GameIsOn = false;
-                        }
-                    }
-                         
-                }  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    //PlayerChoice = DisplayMenu("Restart?", " Yes", " No", "", "", "", "");
+
+                    //    if (PlayerChoice == "1")
+                    //    {
+                    //        GameIsOn = true;
+                    //    }
+                    //    else if (PlayerChoice == "2");
+                    //    {
+                    //        Console.WriteLine("Goodbye");
+                    //        GameIsOn = false;
+                    //    }
+
             }
         }
     }
